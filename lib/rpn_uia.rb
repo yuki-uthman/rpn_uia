58c73e98864afb3fc401fe988d5115712dff1bcd
Yuki Yoshimine <yuki07yuki@gmail.com>
2021-03-25 00:35:29 +0800

    Init repo

---
+++ lib/rpn_uia.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rpn_uia/version"
+
+module RpnUIA
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rpn_uia_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+RSpec.describe RpnUIA do
+  it "has a version number" do
+    expect(RpnUIA::VERSION).not_to be nil
+  end
+
+end
