a34afa258a2baa98208d0a12625aa23c966e8296
Yuki Yoshimine <yuki07yuki@gmail.com>
2021-04-02 02:06:51 +0800

    Refactor method definition in Traceable.define_trace

--- lib/rpn_uia/traceable.rb
+++ lib/rpn_uia/traceable.rb
@@ -14,36 +14,54 @@ module RpnUIA
       values.each do |value|
         attr_accessor value.to_sym
 
-        define_method("#{value}_traces") do
-          instance_eval <<~HERE, __FILE__, __LINE__ + 1
-            @#{value}_traces ||= []
-          HERE
-        end
+        define_traces value
+        define_save   value
+        define_clear  value
+        define_restore value
+        define_previous value
+      end
+    end
 
-        define_method("save_#{value}") do
-          instance_eval <<~HERE, __FILE__, __LINE__ + 1
-            #{value}_traces << #{value}
-          HERE
-        end
+    private
 
-        define_method("clear_#{value}") do
-          instance_eval <<~HERE, __FILE__, __LINE__ + 1
-            #{value}_traces.clear
-          HERE
-        end
+    def define_traces(value)
+      define_method("#{value}_traces") do
+        instance_eval <<~HERE, __FILE__, __LINE__ + 1
+          @#{value}_traces ||= []
+        HERE
+      end
+    end
 
-        define_method("any_previous_#{value}?") do
-          instance_eval <<~HERE, __FILE__, __LINE__ + 1
-            !#{value}_traces.empty?
-          HERE
-        end
+    def define_save(value)
+      define_method("save_#{value}") do
+        instance_eval <<~HERE, __FILE__, __LINE__ + 1
+          #{value}_traces << #{value}
+        HERE
+      end
+    end
 
-        define_method("restore_previous_#{value}") do
-          instance_eval <<~HERE, __FILE__, __LINE__ + 1
-            raise NoTraceFoundError if #{value}_traces.empty?
-            self.#{value} = #{value}_traces.pop
-          HERE
-        end
+    def define_clear(value)
+      define_method("clear_#{value}") do
+        instance_eval <<~HERE, __FILE__, __LINE__ + 1
+          #{value}_traces.clear
+        HERE
+      end
+    end
+
+    def define_previous(value)
+      define_method("any_previous_#{value}?") do
+        instance_eval <<~HERE, __FILE__, __LINE__ + 1
+          !#{value}_traces.empty?
+        HERE
+      end
+    end
+
+    def define_restore(value)
+      define_method("restore_previous_#{value}") do
+        instance_eval <<~HERE, __FILE__, __LINE__ + 1
+          raise NoTraceFoundError if #{value}_traces.empty?
+          self.#{value} = #{value}_traces.pop
+        HERE
       end
     end
   end
