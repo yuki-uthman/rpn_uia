76d15952d6b3a3b3aa6428412731ac8f0806ce52
Yuki Yoshimine <yuki07yuki@gmail.com>
2021-04-30 00:19:40 +0800

    Change Visualizer to accept object and height

--- lib/rpn_uia/visualizer.rb
+++ lib/rpn_uia/visualizer.rb
@@ -7,12 +7,14 @@ module RpnUIA
   # A class to visualize Converter or any other class
   #
   class Visualizer
-    # to_visualize must implement 4 methods
+    attr_writer :object
+
+    # to_visualize object must implement 4 methods
     # #headers
     # #columns
     # #next
     # #back
-    def initialize(to_visualize:)
+    def initialize(to_visualize: nil)
       @object = to_visualize
       @table = Table.new
     end
@@ -25,7 +27,8 @@ module RpnUIA
       @object.back
     end
 
-    def visualize
+    def visualize(height: nil)
+      @table.height = height unless height.nil?
       @table.headers = @object.headers
       @table.columns = @object.columns
       @table.render
--- spec/rpn_uia/visualizer_spec.rb
+++ spec/rpn_uia/visualizer_spec.rb
@@ -24,12 +24,12 @@ RSpec.describe RpnUIA::Visualizer do
   end
 
   describe "#visualize" do
-    context "when it succeeds" do
-      let(:object) do
-        double(headers: %w[Input Ops Output],
-               columns: [[1, 2, 3], [:+], [4]])
-      end
+    let(:object) do
+      double(headers: %w[Input Ops Output],
+             columns: [[1, 2, 3], [:+], [4]])
+    end
 
+    context "when it succeeds" do
       it "displays the state of the object in the table" do
         expected = <<~OUTPUT.rstrip
           ┌───────┬─────┬────────┐
@@ -41,6 +41,50 @@ RSpec.describe RpnUIA::Visualizer do
           └───────┴─────┴────────┘
         OUTPUT
         expect(subject.visualize).to eq expected
+      end
+    end
+
+    context "when the object is switched" do
+      it "displays the state of the new object" do
+        expected = <<~OUTPUT.rstrip
+          ┌───────┬─────┬────────┐
+          │ Input │ Ops │ Output │
+          ├───────┼─────┼────────┤
+          │   3   │     │        │
+          │   2   │     │        │
+          │   1   │  +  │   4    │
+          └───────┴─────┴────────┘
+        OUTPUT
+        expect(subject.visualize).to eq expected
+
+        subject.object = double(headers: %w[Stack], columns: [[1, 2, 3]])
+        expected = <<~OUTPUT.rstrip
+          ┌───────┐
+          │ Stack │
+          ├───────┤
+          │   3   │
+          │   2   │
+          │   1   │
+          └───────┘
+        OUTPUT
+        expect(subject.visualize).to eq expected
+      end
+    end
+
+    context "when the height is given" do
+      it "displays the table with the extra height" do
+        expected = <<~OUTPUT.rstrip
+          ┌───────┬─────┬────────┐
+          │ Input │ Ops │ Output │
+          ├───────┼─────┼────────┤
+          │       │     │        │
+          │       │     │        │
+          │   3   │     │        │
+          │   2   │     │        │
+          │   1   │  +  │   4    │
+          └───────┴─────┴────────┘
+        OUTPUT
+        expect(subject.visualize(height: 5)).to eq expected
       end
     end
   end
