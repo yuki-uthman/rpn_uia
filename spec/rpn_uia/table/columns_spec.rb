9e7971573c12239ae41708d1252ff4b2aece3357
Yuki Yoshimine <yuki07yuki@gmail.com>
2021-04-30 00:21:58 +0800

    Add documentation

--- spec/rpn_uia/table/columns_spec.rb
+++ spec/rpn_uia/table/columns_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe RpnUIA::Table, "#columns=" do
   let(:table) { described_class.new columns: columns }
   let(:columns) { [[1, 2, 3], [1, 2, 3, 4, 5]] }
 
+  # the table is not meant to shrink
   context "when new columns are shorter than the current" do
     it "keeps the current height" do
       table.columns = [[1, 2, 3], [1, 2, 3]]
